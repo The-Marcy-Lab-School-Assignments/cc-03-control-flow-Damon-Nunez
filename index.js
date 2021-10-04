cfbc354a4bd2033daeaa9d53394eb2599a8feab3
Damon-Nunez <damon.nunez@student.uncommonhs.org>
2021-10-04 13:54:58 +0000

    Coding Challenge

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 //1
-function countToTen(){
-  
+function betweenFiveAndTwenty(num){
+  if(num > 5 && num <= 20) {
+  return true
+} else {
+return false
+}
+}
+//2
+const countFromOne = (int) => {
+    for(let i = 1; i < int + 1; i++) {
+        console.log(i);
+    }
 }
 
-//2
-function countFromOne(){
-  
-}
 
 //3
-function countEveryEven(){
-  
+function isNegative(num){
+  if(num < 0){
+      return false
+  }else if(num > 0){
+      return true
+  }
 }
 
 //4
-function countEveryOdd(){
-  
+function countMultipleofFive(arr){
+  for(let i=0; i<arr.length;i++){
+     if(i % 5 === 0){
+         console.log(i);
+     }
+  }
 }
 
 //5 
@@ -23,3 +37,7 @@ function countEvens(){
   
 }
 
+console.log(countFromOne(15));
+console.log(betweenFiveAndTwenty(18));
+console.log(isNegative(-3));
+console.log(countMultipleofFive(1,2,3,4,5,6,7,8,9,10));
